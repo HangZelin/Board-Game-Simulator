8d028e4b39e9b5c2674af474221a6f146da781fa
Hang Zelin <69517383+HangZelin@users.noreply.github.com>
2021-05-30 23:10:08 +0800

    Simple Chess Game

---
+++ boardGameSimulator/Assets/Scripts/MovePlate.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlate : MonoBehaviour
+{
+    public GameObject controller;
+
+    GameObject reference = null;
+
+    //Board Positions
+    int BoardX;
+    int BoardY;
+
+    //false : movement, true: attacking
+    public bool attack = false;
+
+    //Functions
+    public void Start()
+    {
+        if (attack)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f, 1.0f); 
+        }
+    }
+
+    public void OnMouseUp()
+    {
+        controller = GameObject.FindGameObjectWithTag("GameController");
+
+        if (attack)
+        {
+            GameObject cp = controller.GetComponent<Game>().GetPosition(BoardX, BoardY);
+
+            if (cp.name == "white_king") controller.GetComponent<Game>().Winner("Orca");
+            if (cp.name == "black_king") controller.GetComponent<Game>().Winner("Rockets");
+
+            Destroy(cp);
+        }
+
+        controller.GetComponent<Game>().SetPositionEmpty(reference.GetComponent<Chessman>().GetXBoard(),
+            reference.GetComponent<Chessman>().GetYBoard());
+
+        reference.GetComponent<Chessman>().SetXBoard(BoardX);
+        reference.GetComponent<Chessman>().SetYBoard(BoardY);
+        reference.GetComponent<Chessman>().SetCoords();
+
+        controller.GetComponent<Game>().SetPosition(reference);
+
+        controller.GetComponent<Game>().NextTurn();
+
+        reference.GetComponent<Chessman>().DestroyMovePlates();
+    }
+
+    public void SetCoords(int x, int y)
+    {
+        BoardX = x;
+        BoardY = y;
+    }
+
+    public void SetReference(GameObject obj)
+    {
+        reference = obj;
+    }
+
+    public GameObject GetReference()
+    {
+        return reference;
+    }
+
+}
