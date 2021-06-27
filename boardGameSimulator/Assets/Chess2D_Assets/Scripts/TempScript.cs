b97c4711e64f085fe54af578ffe47b63333d25ee
Hang Zelin <69517383+HangZelin@users.noreply.github.com>
2021-06-27 17:04:51 +0800

    Add some changes to the UI

--- boardGameSimulator/Assets/Chess2D_Assets/Scripts/TempScript.cs
+++ boardGameSimulator/Assets/Chess2D_Assets/Scripts/TempScript.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,11 +6,15 @@ public class Chessman : MonoBehaviour
 {
     public GameObject controller;
     public GameObject movePlate; //The new Location.
+    public GameObject stepDialog;//stepWindow UI
+
+    public float aspectRatio = Screen.width * 1.0f / Screen.height;
 
     //Positions
     private int xBoard = -1;
     private int yBoard = -1;
     private int is_attack = -1;
+    private bool is_captured = false;
 
     //Keep track of "black" player or "White" player
     private string player;
@@ -58,13 +62,31 @@ public class Chessman : MonoBehaviour
         }
     }
 
+    public void Update()
+    {
+        if (is_captured)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                int pos_x = (int)(Input.mousePosition.x / 35.0);
+                int pos_y = (int)((Input.mousePosition.y - 115.0) / 35.0);
+                PointMovePlate(pos_x, pos_y);
+                is_captured = false;
+            }
+        }
+        else
+        {
+
+        }
+    }
+
     public void SetCoords()
     {
         float x = xBoard;
         float y = yBoard;
 
-        x *= 0.57f;
-        y *= 0.57f;
+        x = x * 0.57f;
+        y = y * 0.57f;
 
         x -= 2f;
         y -= 2f;
@@ -94,11 +116,19 @@ public class Chessman : MonoBehaviour
 
     public void OnMouseUp()
     {
-        if (!controller.GetComponent<Game>().IsGameOver() && controller.GetComponent<Game>().GetCurrentPlayer() == player)
+        if (!controller.GetComponent<Game>().IsGameOver())
         {
-            DestroyMovePlate();
+            if (controller.GetComponent<Game>().GetCurrentPlayer() == player && !is_captured)
+            {
+                is_captured = true;
 
-            InitiateMovePlates();
+                DestroyMovePlate();
+
+
+                audio_source.PlayOneShot(Sound_Capture, 0.7F);
+
+                InitiateMovePlate();
+            }
         }
 
     }
@@ -122,95 +152,10 @@ public class Chessman : MonoBehaviour
         }
     }
 
-    public void InitiateMovePlates()
+    public void InitiateMovePlate()
     {
-        audio_source.PlayOneShot(Sound_Capture, 0.7F);
-        switch (this.name)
-        {
-            case "black_queen":
-            case "white_queen":
-                LineMovePlate(1, 0);
-                LineMovePlate(0, 1);
-                LineMovePlate(1, 1);
-                LineMovePlate(-1, 0);
-                LineMovePlate(0, -1);
-                LineMovePlate(-1, -1);
-                LineMovePlate(-1, 1);
-                LineMovePlate(1, -1);
-                break;
-            case "black_knight":
-            case "white_knight":
-                LMovePlate();
-                break;
-            case "black_bishop":
-            case "white_bishop":
-                LineMovePlate(1, 1);
-                LineMovePlate(1, -1);
-                LineMovePlate(-1, 1);
-                LineMovePlate(-1, -1);
-                break;
-            case "black_king":
-            case "white_king":
-                SurroundMovePlate();
-                break;
-            case "black_rook":
-            case "white_rook":
-                LineMovePlate(1, 0);
-                LineMovePlate(0, 1);
-                LineMovePlate(-1, 0);
-                LineMovePlate(0, -1);
-                break;
-            case "black_pawn":
-                PawnMovePlate(xBoard, yBoard - 1);
-                break;
-            case "white_pawn":
-                PawnMovePlate(xBoard, yBoard + 1);
-                break;
-        }
-    }
 
-    public void LineMovePlate(int xIncr, int yIncr)
-    {
-        Game sc = controller.GetComponent<Game>();
 
-        int x = xBoard + xIncr;
-        int y = yBoard + yIncr;
-
-        while (sc.PositionOnBoard(x, y) && sc.GetPosition(x, y) == null)
-        {
-            MovePlateSpawn(x, y);
-            x += xIncr;
-            y += yIncr;
-        }
-
-        if (sc.PositionOnBoard(x, y) && sc.GetPosition(x, y).GetComponent<Chessman>().player != player)
-        {
-            MovePlateAttackSpawn(x, y);
-        }
-    }
-
-    public void LMovePlate()
-    {
-        PointMovePlate(xBoard + 1, yBoard + 2);
-        PointMovePlate(xBoard - 1, yBoard + 2);
-        PointMovePlate(xBoard + 2, yBoard + 1);
-        PointMovePlate(xBoard + 2, yBoard - 1);
-        PointMovePlate(xBoard + 1, yBoard - 2);
-        PointMovePlate(xBoard - 1, yBoard - 2);
-        PointMovePlate(xBoard - 2, yBoard + 1);
-        PointMovePlate(xBoard - 2, yBoard - 1);
-    }
-
-    public void SurroundMovePlate()
-    {
-        PointMovePlate(xBoard, yBoard + 1);
-        PointMovePlate(xBoard, yBoard - 1);
-        PointMovePlate(xBoard - 1, yBoard - 1);
-        PointMovePlate(xBoard - 1, yBoard);
-        PointMovePlate(xBoard - 1, yBoard + 1);
-        PointMovePlate(xBoard + 1, yBoard - 1);
-        PointMovePlate(xBoard + 1, yBoard);
-        PointMovePlate(xBoard + 1, yBoard + 1);
     }
 
     public void PointMovePlate(int x, int y)
@@ -231,36 +176,13 @@ public class Chessman : MonoBehaviour
             }
         }
     }
-
-    public void PawnMovePlate(int x, int y)
-    {
-        Game sc = controller.GetComponent<Game>();
-        if (sc.PositionOnBoard(x, y))
-        {
-            if (sc.GetPosition(x, y) == null)
-            {
-                MovePlateSpawn(x, y);
-            }
-        }
-
-        if (sc.PositionOnBoard(x + 1, y) && sc.GetPosition(x + 1, y) != null && sc.GetPosition(x + 1, y).GetComponent<Chessman>().player != player)
-        {
-            MovePlateAttackSpawn(x + 1, y);
-        }
-
-        if (sc.PositionOnBoard(x - 1, y) && sc.GetPosition(x - 1, y) != null && sc.GetPosition(x - 1, y).GetComponent<Chessman>().player != player)
-        {
-            MovePlateAttackSpawn(x - 1, y);
-        }
-    }
-
     public void MovePlateSpawn(int BoardX, int BoardY)
     {
         float x = BoardX;
         float y = BoardY;
 
-        x *= 0.57f;
-        y *= 0.57f;
+        x = x * 0.57f;
+        y = y * 0.57f;
 
         x -= 2f;
         y -= 2f;
@@ -279,8 +201,8 @@ public class Chessman : MonoBehaviour
         float x = BoardX;
         float y = BoardY;
 
-        x *= 0.57f;
-        y *= 0.57f;
+        x = x * 0.57f;
+        y = y * 0.57f;
 
         x -= 2f;
         y -= 2f;
@@ -293,5 +215,19 @@ public class Chessman : MonoBehaviour
         is_attack = 1;
         mpScript.SetReference(gameObject);
         mpScript.SetCoords(BoardX, BoardY);
+
+        *//*        //toggle the window to ask user what to do
+                if (!stepDialog.activeInHierarchy)
+                {
+                   Debug.Log("Toggled!");
+                   stepDialog.SetActive(true);
+                   Time.timeScale = 0f;
+                }
+                else
+                {
+                stepDialog.SetActive(false);
+                Time.timeScale = 1f;
+                }*//*
     }
 }
+*/
